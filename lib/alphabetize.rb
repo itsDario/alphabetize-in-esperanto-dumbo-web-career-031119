4bfb821c697ae6c4502353cb505746cf3f7cacfc
github username <itsDario1@gmail.com>
2019-02-14 21:21:23 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require "pry"
 def alphabetize(arr)
   # code here
+  ea = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" #ESPERANTO_ALPHABET
+  arr.sort_by do |word|
+    word.split('').map do |letter|
+      ea.index(letter)
+    end
+  end
 end
